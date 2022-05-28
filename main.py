f48d1b60352a1a15c2dc861d37dad4a00543f77c
mhsn99 <mahsuntay99@gmail.com>
2022-05-28 16:38:47 +0300

    second commit

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Deep Learning Project 1 - Self driving')
+    print("This is a test for second commit")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
