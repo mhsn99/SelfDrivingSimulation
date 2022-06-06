ca3e775de1b1ed4acd56d44e68e2db978adbb586
mhsn99 <mahsuntay99@gmail.com>
2022-06-06 11:58:58 +0300

    creating model

--- train_simulation.py
+++ train_simulation.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from utlis import *
 from sklearn.model_selection import train_test_split
 
@@ -16,3 +18,26 @@ imagesPath, steering= loadData(path, data)
 X_train, X_val, y_train, y_val = train_test_split(imagesPath, steering, test_size=0.2, random_state=42)
 print("Total training images: ", len(X_train))
 print("Total validation images: ", len(X_val))
+
+# Step 5: Data Augmentation
+# Step 6: Preprocessing
+# Step 7:
+# Step 8:
+
+model = createModel()
+model.summary()
+
+# Step 9:
+history = model.fit(batchGen(X_train,y_train,100,1), steps_per_epoch=100, epochs=10,
+          validation_data=batchGen(X_val, y_val, 100, 0), validation_steps=200)
+
+model.save("model.h5")
+print("Model Saved")
+
+plt.plot(history.history["loss"])
+plt.plot(history.history["val_loss"])
+plt.legend(["Training", "Validation"])
+plt.ylim([0,1])
+plt.title("Loss")
+plt.xlabel("Epoch")
+plt.show()
--- utlis.py
+++ utlis.py
@@ -3,6 +3,14 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
+import matplotlib.image as mpimg
+from imgaug import augmenters as iaa
+import cv2
+import random
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Conv2D, Flatten, Dense
+from tensorflow.python.keras.optimizers import adam_v2
+
 
 def getName(filepath):
     return filepath.split("\\")[-1]
@@ -51,10 +59,73 @@ def loadData(path, data):
 
     for i in range(len(data)):
         indexedData = data.iloc[i]
-        # print(indexedData)
         imagesPath.append(os.path.join(path, 'IMG', indexedData[0]))
         steering.append(float(indexedData[3]))
 
     imagesPath = np.asarray(imagesPath)
     steering = np.asarray(steering)
     return  imagesPath, steering
+
+def augmentImage(imgPath, steering):
+    img = mpimg.imread(imgPath)
+    if np.random.rand() < 0.5:
+        pan = iaa.Affine(translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)})
+        img = pan.augment_image(img)
+    if np.random.rand() < 0.5:
+        zoom = iaa.Affine(scale=(1, 1.2))
+        img = zoom.augment_image(img)
+    if np.random.rand() < 0.5:
+        brightness = iaa.Multiply((0.2, 1.2))
+        img = brightness.augment_image(img)
+    if np.random.rand() < 0.5:
+        img = cv2.flip(img, 1)
+        steering = -steering
+    return img, steering
+
+def preProcess(img):
+    img = img[60:135,:,:]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img,  (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+
+
+def batchGen(imagesPath, steeringList, batchSize, trainFlag):
+    while True:
+        imgBatch = []
+        steeringBatch = []
+
+        for i in range(batchSize):
+            index = random.randint(0, len(imagesPath) - 1)
+
+            if trainFlag:
+                img, steering = augmentImage(imagesPath[index], steeringList[index])
+            else:
+                img = mpimg.imread(imagesPath[index])
+                steering = steeringList[index]
+
+            img = preProcess(img)
+            imgBatch.append(img)
+            steeringBatch.append(steering)
+
+        yield (np.asarray(imgBatch), np.asarray(steeringBatch))
+
+
+def createModel():
+    model = Sequential()
+
+    model.add(Conv2D(24, (5, 5), (2, 2), input_shape=(66, 200, 3), activation='relu'))
+    model.add(Conv2D(36, (5, 5), (2, 2), activation='relu'))
+    model.add(Conv2D(48, (5, 5), (2, 2), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+
+    model.add(Flatten())
+    model.add(Dense(100, activation='elu'))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dense(10, activation='elu'))
+    model.add(Dense(1))
+
+    model.compile(optimizer="adam", loss='mse')
+    return model
